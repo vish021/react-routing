52ebb1e681105a60565a5fe734b29365da0e1ba4
vish021 <vishal.joshi.021@gmail.com>
2018-08-31 12:48:06 -0400

    initial commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+//import axios from 'axios';
+import { Route } from 'react-router-dom';
+import './Blog.css';
+import Posts from './Posts/Posts';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Route path="/" exact component={Posts}/>
+            </div>
+        );
+    }
+}
+
+export default Blog;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+axios.defaults.headers.post['Conent-Type'] = 'Application/json';
+
+axios.interceptors.request.use(requestConfig => {
+    console.log(requestConfig);
+    return requestConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(responseConfig => {
+    console.log(responseConfig);
+    return responseConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
